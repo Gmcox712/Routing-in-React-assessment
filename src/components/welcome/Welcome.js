1d51a38e2f40e76d968c91e2325eeec9c712df26
Garrett4 <gmcox712@gmail.com>
2020-08-31 17:13:14 -0500

    Final commit

---
+++ src/components/welcome/Welcome.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Welcome(props) {
+    return (
+        <div classname="Welcome">
+            Welcome, {props.name}!
+        </div>
+    );
+}
+
+export default Welcome;
